23a17de0892f72d2db16126e8300ae9aede45edc
lemonface88 <123526938+lemonface88@users.noreply.github.com>
2023-06-20 20:44:19 -0400

    Update Resume.jsx

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -135,7 +135,9 @@ function Resume() {
                 
             <div className="flex w-1/ ml-28">
             <a class="hover:animate-bounce mb-2" href="/assets/Harrison(resume).pdf" download="Harrison(resume).pdf">
-            Download Here
+            <svg class ="w-10" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3"></path>
+            </svg>
             </a>
             </div>
 
